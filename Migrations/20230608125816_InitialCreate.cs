392ba752e5237c315d56dc7b96dbe557e10ed697
BnnQ <1v25fa94@gmail.com>
2023-06-09 17:25:47 +0300

    Created initial migration

---
+++ Migrations/20230608125816_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TranslatorTelegramBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ChatId = table.Column<long>(type: "bigint", nullable: false),
+                    SourceLanguage = table.Column<string>(type: "varchar(4)", maxLength: 4, nullable: true),
+                    TargetLanguage = table.Column<string>(type: "varchar(4)", maxLength: 4, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ChatId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
